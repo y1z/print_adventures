1b558f05456178387aba757af0c27f30b991b215
y1z <whyis2cool@gmail.com>
2021-08-09 17:21:41 -0500

    Add struct that contains data the npc's and player use.

---
+++ src/common_character_data.rs
@@ -0,0 +1,8 @@
+use vector2;
+/// represents data that every character (player and npc's) contains in the game.
+struct characterData {
+  m_pos: vector2i,
+  m_health: i32,
+  m_attack: i32,
+  m_defense: i32,
+}
