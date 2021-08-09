352b2e00013098c32a6c6280c147fbd9f70cf15c
y1z <whyis2cool@gmail.com>
2021-08-09 16:24:14 -0500

    Remove the terminal struct from game_grid.

--- src/game_grid.rs
+++ src/game_grid.rs
@@ -1,18 +1,44 @@
-use terminal_info; 
+mod error_types;
+use terminal_size::{terminal_size, Height, Width};
 /// represents a grid in the game
 pub struct gameGrid {
   m_grid: String,
-  m_terminal: terminalData,
+  m_width: u16,
+  m_height: u16,
 }
 
 impl gameGrid {
-  // used to create a grid
-  pub fn create(string: String) -> gameGrid {
-    return gameGrid { m_grid: string ,terminalData::new() };
+  // used to create a grid with custom parameters.
+  pub fn create(string: String, width: u16, height: u16) -> gameGrid {
+    return gameGrid {
+      m_grid: string,
+      m_width: 0,
+      m_height: 0,
+    };
   }
 
   /// used for making a new empty grid.
   pub fn new() -> gameGrid {
-    return gameGrid::create(String::from(""));
+    return gameGrid::create(String::from(""), 0, 0);
+  }
+
+  /// updates the grid.
+  pub fn update(&self){
+    update_terminal_size(self);
+  }
+
+  /// Finds out what the size of the users terminal is.
+  fn update_terminal_size(&self)  {
+    let term_size = terminal_size();
+
+    if let Some((Width(self.m_width), Height(self.m_height))) = term_size {
+      println!(
+        "Your terminal is {} cols wide and {} lines tall",
+        self.m_width, self.m_height
+      );
+      assert_ne!(self.m_width, 0);
+      assert_ne!(self.m_height, 0);
+    } 
+    error_types::PRINT_DEBUG_INFO!();
   }
 }
