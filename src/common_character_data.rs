5a700a160fe18663c67b16292f6218633b345480
y1z <whyis2cool@gmail.com>
2021-08-10 18:43:25 -0500

    Add varaible for character size (in the world).

--- src/common_character_data.rs
+++ src/common_character_data.rs
@@ -2,6 +2,7 @@ use crate::vector2::vector2i;
 /// represents data that every character (player and npc's) contains in the game.
 struct commonCharacterData {
   m_pos: vector2i,
+  m_size: vector2i,
   m_health: i32,
   m_attack: i32,
   m_defense: i32,
