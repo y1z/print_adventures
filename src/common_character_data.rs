8d2cc814cbb086d3340134f38207bc627679f85a
y1z <whyis2cool@gmail.com>
2021-08-12 23:55:39 -0500

    Add a member string for a characters name.

--- src/common_character_data.rs
+++ src/common_character_data.rs
@@ -1,6 +1,7 @@
 use crate::vector2::vector2i;
 /// represents data that every character (player and npc's) contains in the game.
 struct commonCharacterData {
+  m_name: String,
   m_pos: vector2i,
   m_size: vector2i,
   m_health: i32,
