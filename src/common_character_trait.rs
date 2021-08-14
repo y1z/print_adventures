c939e500a7949cff82ef657a9c4f28b7e4e45b42
y1z <whyis2cool@gmail.com>
2021-08-14 03:32:31 -0500

    Added trait for attacking the player and enemy's

--- src/common_character_trait.rs
+++ src/common_character_trait.rs
@@ -1,4 +1,5 @@
-use pra_player;
+use pra_enemy::enemy;
+use pra_player::player;
 use vector2;
 
 /// functions that every character should have.
@@ -7,4 +8,14 @@ pub trait CommonCharacterBahavoir {
   fn get_stats_string(&self) -> String;
   fn get_position(&self) -> vector2i;
   fn get_size(&self) -> vector2i;
+}
+
+/// Trait any character capable of attacking the player
+pub trait AttackPlayer {
+  fn attack_player(&self, _player: &mut pra_player::player);
+}
+
+/// Trait any character capable of attacking an enemy
+pub trait AttackEnemy {
+  fn attack_enemy(&self, _enemy: &mut pra_enemy::enemy);
 }
