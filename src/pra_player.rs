3cc05710108074b8f4e82be152db75e7fd714aad
y1z <whyis2cool@gmail.com>
2021-08-16 02:15:37 -0500

    Add a function for attacking enemy's

--- src/pra_player.rs
+++ src/pra_player.rs
@@ -1,5 +1,16 @@
 use common_character_data;
+use common_character_trait::AttackEnemy;
+use pra_enemy;
 /// Represent the player
 struct player {
   m_data: common_character_data,
+}
+
+impl AttackEnemy for player {
+  fn attack_enemy(&self, _enemy: &mut pra_enemy::enemy) {
+    let damage = self.m_data.m_attack - _enemy.m_data.m_defense;
+    if damage > 0 {
+      _enemy.m_data.m_health = _enemy.m_data.m_health - damage;
+    }
+  }
 }
