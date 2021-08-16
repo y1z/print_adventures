d3f0a1f707ed2d3d5719166d4c5fe9641ddcc356
y1z <whyis2cool@gmail.com>
2021-08-16 02:16:28 -0500

    Add 'Clone' trait to struct

--- src/common_character_data.rs
+++ src/common_character_data.rs
@@ -1,5 +1,6 @@
 use crate::vector2::vector2i;
 /// represents data that every character (player and npc's) contains in the game.
+#[derive(Clone)]
 struct commonCharacterData {
   m_name: String,
   m_pos: vector2i,
