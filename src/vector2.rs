ac49cb720b221554d2652236bff4d80045900d34
y1z <whyis2cool@gmail.com>
2021-08-16 13:14:52 -0500

    Change name of underlying generic type 'vector2' to 'Vector2'.

--- src/vector2.rs
+++ src/vector2.rs
@@ -3,7 +3,7 @@ use std::ops::*;
 
 /// Represents a 2d mathematical vector.
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct vector2<T> {
+pub struct Vector2<T> {
   x: T,
   y: T,
 }
@@ -11,7 +11,7 @@ pub struct vector2<T> {
 ///
 /// Implementations
 ///
-impl<T> vector2<T>
+impl<T> Vector2<T>
 where
   T: BasicArithmetic<T> + SquareRoot<T, T>,
 {
@@ -24,50 +24,50 @@ where
   }
 }
 
-impl<T: Add<Output = T>> Add for vector2<T> {
+impl<T: Add<Output = T>> Add for Vector2<T> {
   type Output = Self;
   fn add(self, other: Self) -> Self::Output {
-    vector2::<T> {
+    Vector2::<T> {
       x: self.x + other.x,
       y: self.y + other.y,
     }
   }
 }
 
-impl<T: Sub<Output = T>> Sub for vector2<T> {
+impl<T: Sub<Output = T>> Sub for Vector2<T> {
   type Output = Self;
   fn sub(self, other: Self) -> Self::Output {
-    vector2::<T> {
+    Vector2::<T> {
       x: self.x - other.x,
       y: self.y - other.y,
     }
   }
 }
 
-impl<T: Mul<Output = T>> Mul for vector2<T> {
+impl<T: Mul<Output = T>> Mul for Vector2<T> {
   type Output = Self;
   fn mul(self, other: Self) -> Self::Output {
-    return vector2::<T> {
+    return Vector2::<T> {
       x: self.x * other.x,
       y: self.y * other.y,
     };
   }
 }
 
-impl<T: Div<Output = T>> Div for vector2<T> {
+impl<T: Div<Output = T>> Div for Vector2<T> {
   type Output = Self;
   fn div(self, other: Self) -> Self::Output {
-    vector2::<T> {
+    Vector2::<T> {
       x: self.x / other.x,
       y: self.y / other.y,
     }
   }
 }
 
-impl<T: Neg<Output = T>> Neg for vector2<T> {
+impl<T: Neg<Output = T>> Neg for Vector2<T> {
   type Output = Self;
   fn neg(self) -> Self::Output {
-    vector2::<T> {
+    Vector2::<T> {
       x: -self.x,
       y: -self.y,
     }
@@ -77,7 +77,7 @@ impl<T: Neg<Output = T>> Neg for vector2<T> {
 /**
  * Pre-made types.
  */
-pub type Vector2u = vector2<u32>;
-pub type Vector2i = vector2<i32>;
-pub type Vector2f = vector2<f32>;
-pub type Vector2d = vector2<f64>;
+pub type Vector2u = Vector2<u32>;
+pub type Vector2i = Vector2<i32>;
+pub type Vector2f = Vector2<f32>;
+pub type Vector2d = Vector2<f64>;
