25a7638900a36446d609a09493d6e9f41b858b28
y1z <whyis2cool@gmail.com>
2021-08-17 13:13:06 -0500

    Add function for calculating function

--- src/pra_player.rs
+++ src/pra_player.rs
@@ -6,9 +6,16 @@ struct player {
   m_data: common_character_data,
 }
 
+impl player {
+  /// calculates the damage the player will do
+  pub fn calculate_damage(enemy: &pra_enemy::enemy) -> i32 {
+    self.m_data.m_attack - nemy.m_data.m_defense
+  }
+}
+
 impl AttackEnemy for player {
   fn attack_enemy(&self, _enemy: &mut pra_enemy::enemy) {
-    let damage = self.m_data.m_attack - _enemy.m_data.m_defense;
+    let damage = self.calculate_damage(_enemy);
     if damage > 0 {
       _enemy.m_data.m_health = _enemy.m_data.m_health - damage;
     }
