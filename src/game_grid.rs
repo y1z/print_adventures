86f47eb072b9a44f3f1f6b9da08adf896495fbed
y1z <whyis2cool@gmail.com>
2021-08-22 01:53:53 -0500

    Created 'terminalInfo' struct for data related with the terminal

--- src/game_grid.rs
+++ src/game_grid.rs
@@ -1,11 +1,21 @@
 use crate::error_types;
+use crate::vector2::Vector2u;
 use terminal_size::{terminal_size, Height, Width};
+
+/// represents the data from the terminal
+#[derive(Debug, Clone)]
+pub struct terminalInfo {
+  m_terminal_width: u16,
+  m_terminal_height: u16,
+}
+
 /// represents a grid in the game
 #[derive(Debug, Clone)]
 pub struct gameGrid {
   m_grid: String,
   m_width: u16,
   m_height: u16,
+  m_term: terminalInfo,
 }
 
 macro_rules! FILL_FORMAT {
@@ -14,13 +24,47 @@ macro_rules! FILL_FORMAT {
   };
 }
 
+impl terminalInfo {
+  fn new() -> terminalInfo {
+    terminalInfo {
+      m_terminal_width: 0,
+      m_terminal_height: 0,
+    }
+  }
+  /// Finds out what the size of the users terminal is.
+  fn get_terminal_size() -> (u16, u16) {
+    let term_size = terminal_size();
+    let mut result: (u16, u16) = (0, 0);
+    if let Some((Width(result_width), Height(result_height))) = term_size {
+      assert_ne!(result_width, 0);
+      assert_ne!(result_height, 0);
+      result = (result_width, result_height);
+    }
+    result
+  }
+
+  /// Set's the terminal size
+  fn set_terminal_size(&mut self, new_term_size: (u16, u16)) {
+    self.m_terminal_height = new_term_size.0;
+    self.m_terminal_width = new_term_size.1;
+  }
+
+  /// Initalizes the 'terminalInfo' struct
+  pub fn init(&mut self) {
+    self.set_terminal_size(terminalInfo::get_terminal_size());
+  }
+}
+
 impl gameGrid {
   /// used to create a grid with custom parameters.
   pub fn create(string: String, width: u16, height: u16) -> gameGrid {
+    let mut terminal_info = terminalInfo::new();
+    terminal_info.init();
     return gameGrid {
       m_grid: string,
       m_width: width,
       m_height: height,
+      m_term: terminal_info,
     };
   }
 
@@ -29,31 +73,14 @@ impl gameGrid {
     return gameGrid::create(String::from(""), 0, 0);
   }
 
-  /// updates the grid.
-  pub fn update(&mut self) {
-    self.update_terminal_size();
-  }
-
-  /// Finds out what the size of the users terminal is.
-  fn update_terminal_size(&mut self) {
-    let term_size = terminal_size();
-    if let Some((Width(temp_width), Height(temp_height))) = term_size {
-      println!(
-        "Your terminal is {} cols wide and {} lines tall",
-        temp_width, temp_height
-      );
-      self.m_width = temp_width;
-      self.m_height = temp_height;
-      assert_ne!(self.m_width, 0);
-      assert_ne!(self.m_height, 0);
-    }
-    error_types::PRINT_DEBUG_INFO!();
+  /// initalizes the game grid
+  pub fn init(&mut self, width: u16, height: u16) {
+    self.m_grid.reserve((width * height) as usize);
+    self.fill_grid();
   }
 
   /// fills the grid with '%' characters .
   pub fn fill_grid(&mut self) {
-    const FILL_ENTIRE_GRID_FORMAT: &'static str = "{:%<-spaces$}";
-    self.update_terminal_size();
     let total_spaces = self.m_width * self.m_height;
     self.m_grid = format!(FILL_FORMAT!(), "", spaces = total_spaces as usize);
   }
